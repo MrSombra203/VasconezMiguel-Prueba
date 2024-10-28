51373b5f91d887602e5c2f4882248a61372e7511
MrSombra203 <jose.vasconez.andrade@udla.edu.ec>
2024-10-28 08:55:34 -0500

    Se coloco un boton y un mensaje al presionarlo.

--- VasconezMiguel-Prueba/wwwroot/js/site.js
+++ VasconezMiguel-Prueba/wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+document.getElementById('boton1').onclick = function () {
+    alert('Mi nombre es miguel y mi honnie son los videojuegos, porque tocas el boton?!');
+}
 // Write your JavaScript code.
