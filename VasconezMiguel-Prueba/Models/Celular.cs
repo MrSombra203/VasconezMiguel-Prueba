12724eac52070e07478fab9977b8f6418774c6b8
MrSombra203 <jose.vasconez.andrade@udla.edu.ec>
2024-10-28 07:59:27 -0500

    Se aghrego todos los modelos y las anotacion resopectivas

---
+++ VasconezMiguel-Prueba/Models/Celular.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VasconezMiguel_Prueba.Models
+{
+    public class Celular
+    {
+        [Key]
+        [Required(ErrorMessage = "Es obligatorio ingresar la ID")]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [DisplayName("Marca")]
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        [AllowNull]
+        public float precio { get; set; }
+    }
+}
---
+++ VasconezMiguel-Prueba/Models/MVasconez.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace VasconezMiguel_Prueba.Models
+{
+    public class MVasconez
+    {
+        [Key]
+        [Required(ErrorMessage = "La ID es obligatorio")]
+        [AllowNull]
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string Nombre { get; set; }
+        [DataType(DataType.Date)]
+        [AllowNull]
+        public DateTime Fecha { get; set; }
+        public float peso { get; set; }
+        public bool Compra {  get; set; }
+        public Celular Celular { get; set; }
+        [ForeignKey("Celular")]
+        public int IdCelular { get; set; }
+
+    }
+}
